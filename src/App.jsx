df5ecfcfdda504e1980143dc6301427fe434c8a2
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-06-29 12:13:04 +0300

    add files

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {useState} from 'react'
+
+export default function App (){
+  const [students , setStudents] = useState([])
+
+  const handleKeyUp = e => { 
+    if(e.keyCode === 13){
+      setStudents([...students , e.target.value])
+      e.target.value = null
+    }
+  }
+
+return(
+  <>
+   <h1>Hello world</h1>
+   <input type="text" onKeyUp={handleKeyUp} />
+   <ul>
+       {
+        students.map((student , index) =>{
+         return (
+        <li key={index}>{student}</li>
+          )
+        })
+       }
+   </ul>  
+  </ >
+  )
+
+}
